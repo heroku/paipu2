bf34b3cbf7118bcf0acc3275b9b51797e6b06f38
Andrew Appleton <andrew@floatleft.com>
2015-02-03 15:30:11 +0000

    asdf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('views', __dirname + '/public');
 
 app.get('/', function(request, response) {
   response.render('index.html')
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
