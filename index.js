f4595fe32672b0b7ed255be3edfc4277c0016040
Andrew Appleton <andrew@floatleft.com>
2015-05-05 16:39:30 +0100

    semicolons

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 
 app.get('/', function(request, response) {
-  response.render('index.html')
-})
+  response.render('index.html');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
